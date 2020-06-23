9643c48d3afb5a0eeeb4fa64943b9a3ae74e8d34
luisguiserrano <luisgui.serrano@gmail.com>
2020-06-23 15:58:47 -0400

    Adding src and templates

---
+++ src/python/xor/functions.py
@@ -0,0 +1,101 @@
+import numpy as np
+import torch
+from torch import nn
+from torch import optim
+import json
+from json import JSONEncoder
+
+# Build the dataset
+def build_dataset():
+    features = np.array([[0,0],[0,1],[1,0],[1,1]]).astype(np.float32)
+    features = torch.from_numpy(features)
+
+    labels = np.array([0,1,1,0]).astype(np.float32).reshape(-1,1)
+    labels = torch.from_numpy(labels)
+
+    dataset = (features, labels)
+    return features, labels
+
+# Build and train the model
+def build_and_train_model(hidden_layer = 8, epochs = 5000, lr = 0.01):
+    
+    # Join the features and labels
+    features, labels = build_dataset()
+    data = [d for d in zip(features, labels)]
+
+    #Build the model
+    model = nn.Sequential(nn.Linear(2, hidden_layer),
+                        nn.ReLU(),
+                        nn.Linear(hidden_layer, 1))
+
+    criterion = nn.BCEWithLogitsLoss()
+    optimizer = optim.SGD(model.parameters(), lr = lr)
+
+    running_losses = []
+
+    # Train the model
+    for e in range(epochs):
+        running_loss = 0
+        for data_point in data:
+            feature = data_point[0]
+            label = data_point[1]
+            optimizer.zero_grad()
+            output = model(feature)
+            loss = criterion(output, label)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+        if e % (epochs/10) == 0:
+            print(f"Epoch: {e}, Training loss: {running_loss}")
+            running_losses.append(running_loss)
+
+    return model
+
+def predict(model, features):
+    sigmoid = nn.Sigmoid()
+    for f in features:
+        print(sigmoid(model(f)))
+    
+def read_json(filename) -> dict:
+    """
+    Loads data from JSON.
+    Args:
+        filename (str): the file to load the data from.
+    Returns:
+        data (dict): data that was loaded from the file.
+    """
+    data = {}
+    try:
+        with open(filename, 'r') as f:
+            data = json.load(f)
+    except IOError:
+            print(f'Error: Could not open {filename}')
+
+    return data
+
+
+def save_json(result, filename) -> None:
+    """
+    Saves data as JSON.
+    Args:
+        result (ditc): of data to save.
+        filenames (str): file name to save the data in
+            (should have a '.json' extension).
+    """
+    try:
+        with open(filename,'w') as f:
+            result["schema"] = "orquestra-v1-data"
+            f.write(json.dumps(result, indent=2)) 
+
+    except IOError:
+        print(f'Error: Could not open {filename}')
+
+
+
+print("Datset:")
+dataset = build_dataset()
+print("\nTraining:")
+model = build_and_train_model()
+features = dataset[0]
+print("\nPredictions:")
+predict(model, features)
---
+++ src/setup.py
@@ -0,0 +1,27 @@
+import os
+import setuptools
+
+readme_path = os.path.join("..", "README.md")
+with open(readme_path, "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name                            = "z-xor",
+    version                         = "0.1.0",
+    author                          = "Zapata Computing, Inc.",
+    author_email                    = "info@zapatacomputing.com",
+    description                     = "XOR with a neural network in Orquestra.",
+    long_description                = long_description,
+    long_description_content_type   = "text/markdown",
+    url                             = "https://github.com/luisguiserrano/xor-orquestra",
+    packages                        = setuptools.find_packages(where = "python"),
+    package_dir                     = {"" : "python"},
+    classifiers                     = (
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ),
+    install_requires = [
+        "torch",
+        "numpy"
+   ],
+)
