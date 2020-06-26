0e2f3d413a0ba07d751ca7feb769064b466ac75e
luisguiserrano <luisgui.serrano@gmail.com>
2020-06-26 16:28:39 -0400

    Fixing json serializations

--- src/python/xor/generate_data.py
+++ src/python/xor/generate_data.py
@@ -1,14 +1,20 @@
 import numpy as np
 import json
-from xor.utils import save_torch
+from xor.utils import serialize_torch
 import torch
 
 # Build the dataset
 def build_dataset():
     features = np.array([[0,0],[0,1],[1,0],[1,1]]).astype(np.float32)
     features = torch.from_numpy(features)
+    serialized_features = serialize_torch("torch_features", features)
 
     labels = np.array([0,1,1,0]).astype(np.float32).reshape(-1,1)
     labels = torch.from_numpy(labels)
+    serialized_labels = serialize_torch("torch_labels", labels)
+    
+    result = {}
+    result["features", [serialized_features]]
+    result["labels", [serialized_labels]]
 
-    return features, labels
+    return result
