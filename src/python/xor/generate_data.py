c3bbd8a105d0d3723449a6e633da49df954d8d7e
luisguiserrano <luisgui.serrano@gmail.com>
2020-07-06 09:57:52 -0400

    Fixing typo in generate_data

--- src/python/xor/generate_data.py
+++ src/python/xor/generate_data.py
@@ -8,13 +8,13 @@ def build_dataset():
     features = np.array([[0,0],[0,1],[1,0],[1,1]]).astype(np.float32)
     features = torch.from_numpy(features)
     serialized_features = serialize_torch("torch_features", features)
-
+    
     labels = np.array([0,1,1,0]).astype(np.float32).reshape(-1,1)
     labels = torch.from_numpy(labels)
     serialized_labels = serialize_torch("torch_labels", labels)
-    
+
     result = {}
-    result["features", serialized_features]
-    result["labels", serialized_labels]
+    result["features"] = serialized_features
+    result["labels"] = serialized_labels
 
     return result
